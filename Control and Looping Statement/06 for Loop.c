e435ba6832a66ec2f7846f18951778a754e83979
Tejswinisutar <tejswinisutar56@gmail.com>
2024-09-19 21:43:51 +0530

    For Loop

---
+++ Control and Looping Statement/06 for Loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Cnt = 0;
+  printf("\n Hello \n");
+  
+  for(Cnt=1;Cnt<=10;Cnt++)
+  {
+    printf("\n %d Welcome",Cnt);
+  }
+  printf("\n\nThank youhh");
+  getch();
+  return 0;
+}
