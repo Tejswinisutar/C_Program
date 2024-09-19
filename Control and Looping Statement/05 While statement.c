a11573a4f01e94d23eb4d5fb44d4af1bba7b8dc1
Tejswinisutar <tejswinisutar56@gmail.com>
2024-09-19 21:37:36 +0530

    While

---
+++ Control and Looping Statement/05 While statement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Cnt = 1;
+  printf("\n Hello");
+  
+  while(Cnt < 11)
+  {
+    printf("\n %d Tejuuu Sutar",Cnt);
+    Cnt++;
+  }
+  printf("\n\n Thank youhh");
+  getch();
+  return 0;
+}
