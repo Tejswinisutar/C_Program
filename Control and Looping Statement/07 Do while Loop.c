94f273202a2edc284701e96353b8ce7d16b59ae6
Tejswinisutar <tejswinisutar56@gmail.com>
2024-09-19 21:50:18 +0530

    Do while loop

---
+++ Control and Looping Statement/07 Do while Loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Cnt = 0;
+  printf("\n Hello \n");
+  Cnt =1;
+  do
+  {
+    printf("\n %d Welcome",Cnt);
+    Cnt++;
+  }
+  while( Cnt<=10);
+  printf("\n\nThank youh");
+  getch();
+  return 0;
+}
