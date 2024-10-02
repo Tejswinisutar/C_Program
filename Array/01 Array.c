4aa7ff887c10527282b4178c71a3bdf33a43ce40
Tejswinisutar <tejswinisutar56@gmail.com>
2024-10-02 21:35:26 +0530

    Program

---
+++ Array/01 Array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Value[5]={};
+  
+  printf("\n Value of 101st elements=%d",Value[0]);
+  printf("\n Value of 102st elements=%d",Value[1]);
+  printf("\n Value of 103rd elements=%d",Value[2]);
+  printf("\n Value of 103th elements=%d",Value[3]);
+  printf("\n Value of 104th elements=%d",Value[4]);
+  getch();
+  return 0;
+}
