9c6a9d941e3c6882e9146d30c4d1e8db93257fda
Tejswinisutar <tejswinisutar56@gmail.com>
2024-10-17 21:39:11 +0530

    Pattern

---
+++ Pattern Printing/pattern 5 by 3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int R=0,C=0;
+  
+  printf("\n ***** Pattern *** \n");
+  
+  for(R=1;R<=5;R++) 
+   {
+     for(C=1;C<=3;C++)
+        {
+          printf(" * ");
+        }
+          printf("\n");
+   }
+   printf("\n Thank you");
+   getch();
+   return 0;
+}
