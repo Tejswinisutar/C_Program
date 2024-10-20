a79f724ec25c68d6f42163a7434814ca46d39d2d
Tejswinisutar <tejswinisutar56@gmail.com>
2024-10-20 22:22:19 +0530

    Array

---
+++ Array/Array with Maloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#define  BillCount 4
+int main()
+{
+  int i=0,Bills[BillCount]={0};
+  
+  printf("\n Enter Todays Bills \n");
+  for(i=0;i<BillCount;i++)
+  {
+    printf("\n Enter Bill No %d",i+1);
+    scanf("%d",Bills[i]);
+  }
+  getch();
+  system ("cls");
+  printf("\n\n Bills on Array Are \n\n");
+  for(i=0;i<BillCount;i++)
+   {
+     printf("\n Value of %d Bill = %d",i+101,Bills[i]);
+   }
+   getch();
+   return 0;
+}
