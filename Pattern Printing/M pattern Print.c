3aade627a08adbbe58681dee793569428143cce1
Tejswinisutar <tejswinisutar56@gmail.com>
2024-11-23 21:09:19 +0530

    M pattern

---
+++ Pattern Printing/M pattern Print.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int i,j,n;
+  
+  printf("\n Enter size of n");
+  scanf("%d",&n);
+  
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<2*n-1;j++)
+    {
+      if(j==0||j==2*n-2||j==i||j==2*n-2-i)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+  getch();
+  return 0;
+}
