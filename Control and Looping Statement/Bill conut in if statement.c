ea02b3e98fa87d3d7152de2b96dec1065beb0de8
Tejswinisutar <tejswinisutar56@gmail.com>
2024-12-28 19:02:12 +0530

    Add files via upload

---
+++ Control and Looping Statement/Bill conut in if statement.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float Bill = 0.0;
+    printf("\n Enter Your Balence");
+    scanf("%f",&Bill);
+
+    if(Bill > 100)
+    {
+        printf("\n You Are Eligible");
+    }
+
+     printf("\n\n Thanks For Visit");
+
+    getch ();
+    return 0;
+
+}
