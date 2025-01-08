896a0853b73f32921f00b865ceccd20e217829aa
Tejswinisutar <tejswinisutar56@gmail.com>
2025-01-08 18:29:19 +0530

    Add files via upload

---
+++ Array/Array 09.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num[7] = {};
+
+    printf("\n Enter Value For Element Number 1 = ");
+    scanf("%d", &Num[0]);
+    printf("\n Enter Value For Element Number 2 = ");
+    scanf("%d", &Num[1]);
+    printf("\n Enter Value For Element Number 3 = ");
+    scanf("%d", &Num[2]);
+    printf("\n Enter Value For Element Number 4 = ");
+    scanf("%d", &Num[3]);
+    printf("\n Enter Value For Element Number 5 = ");
+    scanf("%d", &Num[4]);
+    printf("\n Enter Value For Element Number 6 = ");
+    scanf("%d", &Num[5]);
+    printf("\n Enter Value For Element Number 7 = ");
+    scanf("%d", &Num[6]);
+
+    printf("\n Values Of Element 1= %d.",Num[0]);
+    printf("\n Values Of Element 1= %d.",Num[1]);
+    printf("\n Values Of Element 1= %d.",Num[2]);
+    printf("\n Values Of Element 1= %d.",Num[3]);
+    printf("\n Values Of Element 1= %d.",Num[4]);
+    printf("\n Values Of Element 1= %d.",Num[5]);
+    printf("\n Values Of Element 1= %d.",Num[6]);
+
+
+  getch ();
+  return 0;
+}
