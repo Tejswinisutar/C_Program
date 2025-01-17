12174502351a12af3a50754f36f6a9b18aa304ae
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-01-17 20:41:16 +0530

    Add files via upload

---
+++ Array_Assignments/02_Array With Inialization &Display.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int value[5]={12,13,14,15,16};
+
+    printf("\n value of 101st element=%d",value[0]);
+    printf("\n value of 102nd element=%d",value[1]);
+    printf("\n value of 103rd element=%d",value[2]);
+    printf("\n value of 104th element=%d",value[3]);
+    printf("\n value of 105th element=%d",value[4]);
+
+    getch();
+    return 0;
+}
