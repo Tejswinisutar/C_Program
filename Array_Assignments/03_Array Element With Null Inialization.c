97f9e3e0489dc5738e0828a33be2575c8ff32b5b
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-01-17 20:41:39 +0530

    Add files via upload

---
+++ Array_Assignments/03_Array Element With Null Inialization.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Num[7]={0};
+
+  printf("\n Enter a 1 elements");
+  scanf("%d",&Num[0]);
+  printf("\n Enter a 2 elements");
+  scanf("%d",&Num[1]);
+  printf("\n Enter a 3 elements");
+  scanf("%d",&Num[2]);
+  printf("\n Enter a 4 elements");
+  scanf("%d",&Num[3]);
+  printf("\n Enter a 5 elements");
+  scanf("%d",&Num[4]);
+  printf("\n Enter a 6 elements");
+  scanf("%d",&Num[5]);
+  printf("\n Enter a 7 elements");
+  scanf("%d",&Num[6]);
+  getch();
+ printf("\n value of elements 1 %d",Num[0]);
+ printf("\n value of elements 2 %d",Num[1]);
+ printf("\n value of elements 3 %d",Num[2]);
+ printf("\n value of elements 4 %d",Num[3]);
+ printf("\n value of elements 5 %d",Num[4]);
+ printf("\n value of elements 6 %d",Num[5]);
+ printf("\n value of elements 7 %d",Num[5]);
+  return 0;
+}
