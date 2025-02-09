7141fcd6d64799db5f5e1b4153f12bbb5827cf56
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-02-09 13:06:06 +0530

    Add files via upload

---
+++ Basic/01_Table.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+
+    printf("\n Multiplication table for 24 \n");
+
+    for(int i=1; i<=10; i++)
+    {
+        printf("\n %d",24*i);
+    }
+    printf("\n Thank youh...");
+    getch();
+    return 0;
+}
