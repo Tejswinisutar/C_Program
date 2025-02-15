8391594abb7b1a3af1a470297bc3a9a663ffbabc
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-02-15 22:21:35 +0530

    Bitwise Operator

---
+++ Bitwise operator/10_Program to accept a number OFF 7th bit And Return modified number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+typedef long long int LLI;
+
+LLI OFF_7th_Bit(LLI);
+
+int main();
+{
+  LLI No = 0, Ret = 0;
+
+  printf("\n Enter A Number");
+  scanf("%d",&)
+}
