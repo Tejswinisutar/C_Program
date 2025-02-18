05bbcb3b2723dabb2c0ab69ebcdaa6e19797ae4a
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-02-18 09:30:53 +0530

    Pattern

---
+++ Pattern Printing/12_Print Rombus.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+ {
+    int Row = 0, Col = 0, N = 0;
+
+    printf("Enter N number: ");
+    scanf("%d", &N);
+
+    for (Row = 1; Row <= N; Row++)
+     {
+        for (Col = 1; Col <= N; Col++)
+         {
+            if (Row == 1 || Row == N || Col == 1 || Col == N)
+            {
+                 printf("* ");
+            }
+             else
+              {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
