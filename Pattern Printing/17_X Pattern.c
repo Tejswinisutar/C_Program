7cac60f13b103624deb2d4628d5cfa43ae703def
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-02-19 18:38:14 +0530

    Pattern

---
+++ Pattern Printing/17_X Pattern.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+  int No=0,i=0,j=0;
+
+  printf("\n Enter Number");
+  scanf("%d",&No);
+
+  printf("\n ***** X pattern *****");
+
+  for(i=1;i<=No;i++)
+   {
+     for(j=1;j<=No;j++)
+      {
+        if(i+j==No+1||i==j)
+        {
+          printf("*");
+        }
+        else
+        {
+          printf(" ");
+        }
+      }
+      printf("\n");
+   }
+   printf("\n ***** Thank you ****");
+   getch();
+   return 0;
+}
