973c01617fb2737f65f8b32f72148f8b372a76f5
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-02-22 20:16:18 +0530

    Pattern

---
+++ Pattern Printing/Pattern_Printting_Assignments/02_Alternate Symbol.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0,j = 0,R = 0,C = 0;
+
+    printf("\n Enter Row Size:");
+    scanf("%d",&R);
+    printf("\n Enter Column Size:");
+    scanf("%d",&C);
+
+    for(i=1;i<=R;i++)
+    {
+      for(j=1;j<=C;j++)
+      {
+          if(j%2==1)
+          {
+            printf(" * ");
+          }
+          else
+          {
+            printf(" # ");
+          }
+      }
+      printf("\n");
+    }
+    getch();
+    return 0;
+}
