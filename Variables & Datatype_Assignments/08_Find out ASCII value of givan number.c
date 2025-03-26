d7bdc7b2c08169becc9cb04464dd138398fe4b9b
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-26 22:54:04 +0530

    ASCII value

---
+++ Variables & Datatype_Assignments/08_Find out ASCII value of givan number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+
+    printf("\n Enter a character:");
+    scanf("%c",&ch);
+
+    printf("\n The ASCII value of '%c' is %d\n", ch,ch);
+
+    return 0;
+}
