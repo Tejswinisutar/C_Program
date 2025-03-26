e6b94ebe7634b73c1979aebae29911bad2d48262
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-26 22:53:16 +0530

    Size of value

---
+++ Variables & Datatype_Assignments/06_program Size int ,float ,double.c.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n Hello World");
+
+       int short No1 = 1;
+       int No2 = 2;
+       int long No3 = 3;
+       int long long No4 =4;
+       float Per = 0.0;
+       double No5 = 10.0;
+       char Karad[5]={};
+
+       printf("\n\n size of short %lu",sizeof(short));
+       printf("\n\n size of int %lu",sizeof(int));
+       printf("\n\n size of short %lu",sizeof(long));
+       printf("\n\n size of long long  %lu",sizeof(long long));
+       printf("\n\n size of double %lu",sizeof(double));
+       printf("\n\n size of char %lu\n",sizeof(char));
+
+       return 0;
+}
+
