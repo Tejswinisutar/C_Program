e69f21d03ac0f734e5d5aa98894ae2e410a7a488
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-03-27 22:59:33 +0530

    circle

---
+++ Variables & Datatype_Assignments/12_Circumference of Circle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  float Rad = 0.0,Circum = 0.0;
+  const float PI = 3.14;
+
+  printf("\n Enter Radius of Circle");
+  scanf("%f",&Rad);
+
+  Circum = 2 * PI * Rad;
+
+  printf("\n Circumference of Circle %0.2f=%0.2f",Rad,Circum);
+  getch();
+  return 0;
+}
