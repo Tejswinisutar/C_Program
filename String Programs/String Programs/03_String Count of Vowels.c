96d64f395a404e845e54d29d065106c5467f6e92
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-05-12 19:50:19 +0530

    String Count of Vowels

---
+++ String Programs/String Programs/03_String Count of Vowels.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int Vcnt = 0,i = 0;
+  char cSrc[20] = {'\0'};
+
+  printf("\n Enter A String :");
+  gets(cSrc);
+
+  while(cSrc[i] != '\0')
+  {
+     if(cSrc[i] == 'a' || cSrc[i] == 'e' || cSrc[i] == 'i' || cSrc[i] == 'o' || cSrc[i] == 'u' || cSrc[i] == 'A' || cSrc[i] == 'E' || cSrc[i] == 'I' || cSrc[i] == 'O' || cSrc[i] == 'U')
+     {
+        Vcnt++;
+     }
+   i++;
+  }
+  printf("\n Count of Vowels in Given String is =%d.",Vcnt);
+
+  getch();
+  return 0;
+}
